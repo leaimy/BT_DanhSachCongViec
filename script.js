feb445e5b3c2076de25ae0e2930080624e8f02fe
leaimy <hantctk42@gmail.com>
2020-08-14 21:31:08 +0700

    Update file js

---
+++ script.js
@@ -0,0 +1,29 @@
+var dsCongViec = document.getElementById("danhsach");
+
+function themCongViec(tenCongViec) {
+  var congviec = document.createElement("li");
+  congviec.classList.add("dscon");
+  congviec.innerHTML = `<span>${tenCongViec}</span>
+        <span class="nutbam">
+          <button class="nutbam_1"><i class="fas fa-check"></i></button>
+          <button class="nutbam_2"><b>X</b></button>
+        </span>`;
+  // Thêm sự kiện
+  congviec.addEventListener("click", function () {
+    clickCongViec(congviec);
+  });
+  return congviec;
+}
+
+var congviec = themCongViec("Tập thể dục");
+var congviec1 = themCongViec("Học làm web");
+var congviec2 = themCongViec("Đọc sách");
+var congviec3 = themCongViec("Làm việc nhà");
+var congviec4 = themCongViec("Xem phim");
+
+dsCongViec.appendChild(congviec);
+dsCongViec.append(congviec1, congviec2, congviec3, congviec4);
+
+function clickCongViec(congviec) {
+  congviec.classList.toggle("danhdau");
+}

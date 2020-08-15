12a14d438e99687926247060e88d22d6e40e6a47
leaimy <hantctk42@gmail.com>
2020-08-15 21:16:18 +0700

    Update file js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var dsCongViec = document.getElementById("danhsach");
+var bieumau = document.getElementById("bieumau");
+var input = document.getElementById("input");
 
 var danhsach = [
   "Tập thể dục",
@@ -14,6 +16,11 @@ function themCongViec(tenCongViec) {
   function clickCongViec(congviec) {
     congviec.classList.toggle("danhdau");
   }
+
+  function click(nutbam) {
+    nutbam.classList.toggle("tick");
+  }
+
   var congviec = document.createElement("li");
   congviec.classList.add("dscon");
   congviec.innerHTML = `<span>${tenCongViec}</span>
@@ -21,10 +28,14 @@ function themCongViec(tenCongViec) {
           <button class="nutbam_1"><i class="fas fa-check"></i></button>
           <button class="nutbam_2"><b>X</b></button>
         </span>`;
-  // Thêm sự kiện
+  //Thêm sự kiện
   congviec.addEventListener("click", function () {
     clickCongViec(congviec);
   });
+  var nutbam = document.getElementsByClassName("nutbam_1[0]");
+  // nutbam.addEventListener("click", function () {
+  //   click(nutbam);
+  // });
 
   return congviec;
 }
@@ -35,3 +46,16 @@ function themVaoDanhSach() {
     dsCongViec.appendChild(congviec);
   }
 }
+
+bieumau.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var congViecMoi = input.value;
+  if (congViecMoi.trim().length > 0) {
+    danhsach.push(congViecMoi);
+    dsCongViec.innerHTML = "";
+    themVaoDanhSach();
+
+    input.value = "";
+  }
+});
